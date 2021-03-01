bdac4b99c1338899b1ddc8b6661579a4e5adbca2
Aarish Rahman <aarish@Levi.local>
2021-03-01 20:08:42 +0530

    first commit

---
+++ eggtimer1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  eggtimer1
+//
+//  Created by Aarish Rahman on 28/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func eggButton(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ eggtimer1Tests/eggtimer1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  eggtimer1Tests.swift
+//  eggtimer1Tests
+//
+//  Created by Aarish Rahman on 28/02/21.
+//
+
+import XCTest
+@testable import eggtimer1
+
+class eggtimer1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
